5846d1090d24ed0f53de8c08c1b7eb6ebd44d26d
rauf tababilov <tababilovrauf@gmail.com>
2020-05-08 13:59:53 +0500

    using AsyncTask

--- app/src/main/java/com/example/vkinfo/MainActivity.java
+++ app/src/main/java/com/example/vkinfo/MainActivity.java
@@ -2,20 +2,43 @@ package com.example.vkinfo;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 
+import java.io.IOException;
 import java.net.URL;
 
 import static com.example.vkinfo.utils.NetworkUtils.generateURL;
+import static com.example.vkinfo.utils.NetworkUtils.getResponseFromURL;
 
 public class MainActivity extends AppCompatActivity {
     private EditText searchField;
     private Button searchButton;
     private TextView result;
+
+    class VKQueryTask extends AsyncTask<URL, Void, String> {
+
+        @Override
+        protected String doInBackground(URL... urls) {
+            String response = null;
+            try {
+                response = getResponseFromURL(urls[0]);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+
+            return response;
+        }
+
+        @Override
+        protected void onPostExecute(String response) {
+            result.setText(response);
+        }
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -30,7 +53,8 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 URL generatedURL = generateURL(searchField.getText().toString());
-                result.setText(generatedURL.toString());
+
+                new VKQueryTask().execute(generatedURL);
             }
         };
 
